a2fc05b88f1e9cbe695f454f522a504db6f5d17c
Alpha-Two <alpha2backupemail@gmail.com>
2021-10-11 17:31:20 +0100

    added stuff

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ class schemC {
     constructor(sizeX, sizeY) {
         this.sizeX = sizeX
         this.sizeY = sizeY
+        return
     }
 }
 
+class cell {
+    x = 0;
+    y = 0;
+    t = null;
+    constructor(x, y, t) {
+        this.x = x;
+        this.y = y;
+        this.t = t;
+    }
+}
 exports.schematicClass = schemC
+exports.types = {
+    generator: 0
+}
